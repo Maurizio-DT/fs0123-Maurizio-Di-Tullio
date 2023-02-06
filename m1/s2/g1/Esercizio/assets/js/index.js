baed2f48b8a533ab6cce7e6e46d01f1dd4c16440
Maurizio-DT <m.tu@live.it>
2023-02-06 17:07:03 +0100

    Esercizio s2g1

---
+++ m1/s2/g1/Esercizio/assets/js/index.js
@@ -0,0 +1,19 @@
+/*
+alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
+
+*/
+
+var color = prompt('Scegli il tuo colore preferito');
+//window.alert('Sto studiando JS');
+if(color == "rosso"){
+   document.getElementById("contenitore").style.backgroundColor = "red";
+}else if(color == "verde"){
+    document.getElementById("contenitore").style.backgroundColor = "green";
+}else if(color == "blu"){
+    document.getElementById("contenitore").style.backgroundColor = "blue";
+}else {
+    document.write("Scegli tra rosso, verde o blu, siamo poveri :(")
+}
+Footer
+© 2023 GitHub, Inc.
---
+++ m1/s2/g1/basi/js/script.js
@@ -0,0 +1,10 @@
+/*
+    Commento su
+    più righe
+*/
+
+// Commento su sinogla riga
+
+// alert("Hello World!");
+
+
